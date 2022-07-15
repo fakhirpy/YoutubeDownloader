91b0f6297dbab3295887b3680b346ac20478c5e8
Fakhir <trjakira@gmail.com>
2022-07-15 15:47:00 -0300

    Ytb videos downloader high resolution

--- ytbdown.py
+++ ytbdown.py
@@ -2,6 +2,7 @@ import pytube
 
 url = input("Url video:")
 
+# to use in termux or linux and the directory is not "C:" change to the desired location.
 path="C:"
 
 pytube.YouTube(url).streams.get_highest_resolution().download(path)
